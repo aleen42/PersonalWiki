51906aa366217d091107508f51ad785c2583bc68
Aleen <408503598@qq.com>
2015-05-21 16:43:27 +0800

    update js

---
+++ Programming/JavaScript/Type/Type.js
@@ -0,0 +1,16 @@
+var x;
+x = 0;
+x	//=>0
+
+//JavaScript supports several types of values
+x = 1;	//integers
+x = 0.1;	//real
+x = "Aloha";	//String in quatation marks
+x = 'Aloha';	//String in single quatations 
+x = true;	//boolean
+x = null;	//no value
+x = undefined;	//like null
+
+Math.pow(2,53)	//=>2^53
+Math.round(.6)	//=>1.0
+//...
