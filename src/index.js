564051dfbb74603a02eb0a23f4ee527e0f8d7dce
Aleen <aleen42@vip.qq.com>
2016-04-11 12:16:24 +0800

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+function getScript(src) {
+    var scriptObj = document.createElement('script');
+    scriptObj.src = src;
+    scriptObj.type = 'text/javascript';
+}
+
 window.onload = function () {
     var particles = document.createElement('div');
     particles.setAttribute('id', 'particles-js');
