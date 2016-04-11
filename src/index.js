e3764a03da20e8d32a74b7d73debabbbee56e8ed
Aleen <aleen42@vip.qq.com>
2016-04-11 12:02:40 +0800

    Update src/index.js
    Former-commit-id: e3b1ce66b45e078af1dc7ca7c68a2b7dcf40a806

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 (function init() {
-    var particles = document.createElement('div');
+    var particles = document.body.createElement('div');
     particles.setAttribute('id', 'particles-js');
     document.body.appendChild(particles);
 })();
