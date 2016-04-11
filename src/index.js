e8b43c3f54a94245e847873c707588cbba54f026
Aleen <aleen42@vip.qq.com>
2016-04-11 13:15:20 +0800

    Update src/index.js
    Former-commit-id: 9f96f8eafa59d4896f65d8899b6946e435a910d5

--- src/index.js
+++ src/index.js
@@ -2,15 +2,6 @@ function getScript(src) {
     $(document.body).append('<script src="' + src + '" type="text/javascript"></script>');
 }
 
-window.onload = function () {
-    var particles = document.createElement('div');
-    particles.setAttribute('id', 'particles-js');
-    document.body.appendChild(particles);
-    
-    getScript('src/jquery-2.1.4.min.js');
-    getScript('src/application.js');
-};
-
 $(document).ready(function () {
     $('.book').append('<div id="particles-js"></div>');
 });
