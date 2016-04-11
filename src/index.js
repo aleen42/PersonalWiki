dfb6e1638e3b7517e06b8f9736eb45b58d730d82
Aleen <aleen42@vip.qq.com>
2016-04-11 12:16:37 +0800

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ function getScript(src) {
     var scriptObj = document.createElement('script');
     scriptObj.src = src;
     scriptObj.type = 'text/javascript';
+    document.body.appendChild(scriptObj);
 }
 
 window.onload = function () {
