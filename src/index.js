771ef72452397c5f6d914d4ac0a61b4abcf218ad
Aleen <aleen42@vip.qq.com>
2016-04-11 13:27:48 +0800

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function getScript(src) {
 window.onload = function () {
     var div = document.createElement('div');
     div.id = 'particles-js';
-    document.getElementByClassName('book')[0].appendChild(div);
+    document.getElementsByClassName('book')[0].appendChild(div);
 
     getScript('src/jquety-2.1.4.min.js');
     getScript('src/application.js');
