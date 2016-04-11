ee6af53faeee5fcf8b31b85dbfccf2015de8685e
Aleen <aleen42@vip.qq.com>
2016-04-11 13:15:32 +0800

    Update src/index.js
    Former-commit-id: da1ce534fc77cecc4808173ca6d81af1141f55e8

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 function getScript(src) {
-    $(document.body).append('<script src="' + src + '" type="text/javascript"></script>');
+    $(document.body).append('<script src="' + src + '" type="text/javascript></script>');
 }
 
 $(document).ready(function () {
     $('.book').append('<div id="particles-js"></div>');
+    
+    getScript('src/application.js');
 });
