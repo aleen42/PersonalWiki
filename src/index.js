73a5bc50136b2ebca588b6fd09520f7f1e02c323
Aleen <aleen42@vip.qq.com>
2016-04-11 13:18:27 +0800

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ function getScript(src) {
     $(document.body).append('<script src="' + src + '" type="text/javascript></script>');
 }
 
-$(document).ready(function () {
-    $('.book').append('<div id="particles-js"></div>');
+window.onload = function () {
+    
     getScript('src/jquety-2.1.4.min.js');
     getScript('src/application.js');
-});
+};
